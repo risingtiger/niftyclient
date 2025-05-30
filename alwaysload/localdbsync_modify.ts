6a84950f979169a14bd1c2aa64dd766c792f236e
Davis Hammon <accounts@risingtiger.com>
2025-05-30 14:07:22 -0600

    refactor: improve patch operation handling and sync deduplication

--- alwaysload/localdbsync_modify.ts
+++ alwaysload/localdbsync_modify.ts
@@ -7,15 +7,15 @@ declare var $N: $NT
 
 type OperationTypeT = 'add' | 'patch' | 'delete';
 type PendingSyncOperationT = {
+	id: str;
 	operation_type: OperationTypeT;
 	target_store: str;
-	docId: str;
 	ts: num;
 	oldts: num;
 	payload: GenericRowT | null;
 };
 
-const SYNC_INTERVAL_MINUTES = 5
+const SYNC_INTERVAL_MINUTES = 1
 const SYNC_INTERVAL_MS = SYNC_INTERVAL_MINUTES * 60 * 1000
 const STORAGE_KEY = 'localdbsync_last_run'
 	
@@ -25,6 +25,8 @@ const PENDING_SYNC_STORE_NAME = '__pending_sync_operations';
 const INITIAL_SYNC_INTERVAL = 5000; 
 const MAX_SYNC_INTERVAL = 1800000; // 30 minutes
 const BACKOFF_FACTOR = 1.5; 
+
+const _patches_awaiting_fetchlassie = new Set<string>() 
 
 let _pending_sync_operations_count = -1; // -1 means not set yet -- initially in app pull from indexeddb to find out how many pending sync operations there are
 let _current_sync_interval = INITIAL_SYNC_INTERVAL;
@@ -47,13 +49,15 @@ const Add = (objecstorepath: PathSpecT, data: GenericRowT) => new Promise<string
 	try { db = await $N.IDB.GetDB(); }
 	catch { main_rej("no db connection"); return; }
 
+	data.ts = Math.floor(Date.now() / 1000)
+	data.id = crypto.randomUUID();
 
 	let aye_errs = false
 	const cname = objecstorepath.syncobjectstore.name
 	const tx: IDBTransaction = db.transaction([cname], "readwrite", { durability: "relaxed" })
 	const objectstore = tx.objectStore(cname)
 
-	process_data(data, null)
+	data.ts = Math.floor(Date.now() / 1000)
 
 	try   { await $N.IDB.AddOne_S(objectstore, data); } 
 	catch { aye_errs = true; }
@@ -80,11 +84,14 @@ const Add = (objecstorepath: PathSpecT, data: GenericRowT) => new Promise<string
 
 
 
-const Patch = (path: PathSpecT, data: GenericRowT) => new Promise<num>(async (main_res, main_rej) => {
+const Patch = (path: PathSpecT, newdata: GenericRowT) => new Promise<num>(async (main_res, main_rej) => {
 
 	// at some point would like to patch multiple docs at once
 
-	const db                 = await $N.IDB.GetDB()
+	let db:any
+
+	try { db = await $N.IDB.GetDB(); }
+	catch { main_rej("no db connection"); return; }
 
 	let   oldts              = 0
 	const cname              = path.syncobjectstore.name;
@@ -95,11 +102,16 @@ const Patch = (path: PathSpecT, data: GenericRowT) => new Promise<num>(async (ma
 	try   { olddata = await $N.IDB.GetOne_S(objectStore, path.docid!); }
 	catch { main_rej(); return; }
 
+
+	for (const key in olddata) {
+		if (newdata[key] !== undefined)    olddata[key] = newdata[key]
+	}
+
+	newdata.ts = Math.floor(Date.now() / 1000)
+
 	oldts = olddata.ts;
 
-	process_data(olddata, data); 
-
-	try   { await $N.IDB.PutOne_S(objectStore, data); }
+	try   { await $N.IDB.PutOne_S(objectStore, newdata); }
 	catch { main_rej(); return; }
 
 	try { await $N.IDB.TXResult(tx); } catch { main_rej(); return; }
@@ -107,13 +119,31 @@ const Patch = (path: PathSpecT, data: GenericRowT) => new Promise<num>(async (ma
 	main_res(1);
 
 
-	const body = { cname: path.collection, data, oldts }
+	if (_patches_awaiting_fetchlassie.has(path.path)) {
+		// if we are already awaiting a fetchlassie call for this path, do not make another one
+		// this is to prevent multiple calls to the server for the same patch operation
+
+		await record_failed_sync_operation('patch', cname, newdata.id, oldts, newdata);
+		return;
+	}
+
+	_patches_awaiting_fetchlassie.add(path.path);
+
+	const body = { cname: path.collection, data: newdata, oldts }
 	const opts:  { method: 'POST', body: string } = {method: "POST", body: JSON.stringify(body)};
 
 	const r = await $N.FetchLassie('/api/firestore_patch', opts, null)
-	if (r.ok) { return; }
-
-	await record_failed_sync_operation('patch', cname, data.id, oldts, data);
+	if (r.ok) { 
+		_patches_awaiting_fetchlassie.delete(path.path);
+		return; 
+	}
+	else {
+		// we test here because while the first fetchlassie was happening another patch operation could have been added
+		if (!_patches_awaiting_fetchlassie.has(path.path)) {
+			await record_failed_sync_operation('patch', cname, newdata.id, oldts, newdata);
+		}
+		_patches_awaiting_fetchlassie.delete(path.path);
+	}
 });
 
 
@@ -168,12 +198,37 @@ const SetupLocalDBSyncPeriodic = () => {
 	}
 	
 	// Check immediately on init
-	setTimeout(run_sync_if_needed, 5000)
+	setTimeout(run_sync_if_needed, 3000)
 	
 	// Set up periodic checking
 	setInterval(run_sync_if_needed, SYNC_INTERVAL_MS)
 }
 
+
+
+
+/*
+const process_data = (data: GenericRowT, newpatchdata:GenericRowT|null) => {
+
+	for (const key in data) {
+		if (newpatchdata && newpatchdata[key]) { // only applicable if this is a patch operation
+			data[key] = newpatchdata[key]
+		}
+
+		if (key.endsWith('__ref')) {
+			const baseKey = key.split('__ref')[0]
+			const pathValue = data[key] as str
+			const pathParts = pathValue.split('/')
+			const collection = pathParts.slice(0, -1).join('/')
+			const docId = pathParts[pathParts.length - 1]
+			data[baseKey] = { __path: [collection, docId] }
+		}
+	}
+
+	if (!data.id)   data["id"] = crypto.randomUUID();
+	data["ts"] = Math.floor(Date.now() / 1000)
+}
+*/
 
 
 
@@ -185,15 +240,21 @@ const run_local_db_sync_periodic = async (is_retry_of_max_count_reached:boolean 
 
 	const count = await $N.IDB.Count(PENDING_SYNC_STORE_NAME).catch(() => 0)
 
-	if (!is_retry_of_max_count_reached && count > MAX_PENDING_COUNT) {
+	if (count === 0) {
+		localStorage.removeItem("pending_sync_operations_exists");
+		localStorage.removeItem("pending_sync_operations_too_many");
+		res(true)
+		return
+
+	}
+	else if (!is_retry_of_max_count_reached && count > MAX_PENDING_COUNT) {
 		localStorage.setItem("pending_sync_operations_too_many", "true");
 		$N.Unrecoverable("Sync Error", "App is Offline. Connect to WiFi or Cellular", "Retry Connection", LoggerSubjectE.localdbsync_error_toomany_pending, "localdbsync_pending_too_many", null)		
 		res(false)
 		return
+
 	}
-
-
-	if (count > 500) {
+	else if (count > 500) {
 
 		// this is a safety net to prevent too many pending sync operations and clogging the database and/or server calls
 		// but it is a loss of data, so hopefully this never happens. 
@@ -237,7 +298,7 @@ const run_local_db_sync_periodic = async (is_retry_of_max_count_reached:boolean 
 
 
 const Check_After_Too_Many_Pending_Operations = async (cb:(r:boolean)=>void) => {
-	const r = await RunLocalDBSyncPeriodic(true)
+	const r = await run_local_db_sync_periodic(true)
 	cb(r)
 }
 
@@ -247,56 +308,38 @@ const Check_After_Too_Many_Pending_Operations = async (cb:(r:boolean)=>void) => 
 const record_failed_sync_operation = (
 	type: OperationTypeT,
 	target_store: string,
-	docId: string,
+	docid: string,
 	oldts: num,
 	payload: GenericRowT|null): Promise<void> => new Promise(async (res, rej) => {
 
+	let db:IDBDatabase;
+	try { db = await $N.IDB.GetDB(); }
+	catch { rej(); return; }
+
+	const tx = db.transaction(PENDING_SYNC_STORE_NAME, "readwrite")
+	const s  = tx.objectStore(PENDING_SYNC_STORE_NAME)
+
+	const req = s.get(docid)
+
 	const pendingOp: PendingSyncOperationT = {
+		id:docid,
 		operation_type: type,
 		target_store: target_store,
-		docId,
-		ts: payload ? payload.ts : Math.floor(Date.now() / 1000),
+		ts: payload ? payload.ts : Math.floor(Date.now() / 1000), // if delete set a ts
 		oldts: oldts,
 		payload, 
 	}
 
 	let r:any
 
-	try   { r = await $N.IDB.AddOne(PENDING_SYNC_STORE_NAME, pendingOp); }
+	// if record already is here it will be overwritten.
+	try   { r = await $N.IDB.PutOne(PENDING_SYNC_STORE_NAME, pendingOp); }
 	catch { rej(); return; }
 
 	localStorage.setItem("pending_sync_operations_exists", "true");
 
 	res()
 })
-
-
-
-
-const process_data = (data: GenericRowT, newpatchdata:GenericRowT|null) => {
-
-	for (const key in data) {
-		if (Object.prototype.hasOwnProperty.call(data, key)) {   
-
-			if (newpatchdata && newpatchdata[key]) {
-				data[key] = newpatchdata[key]
-			}
-
-			if (key.endsWith('__ref')) {
-				const baseKey = key.split('__ref')[0]
-				const pathValue = data[key] as str
-				const pathParts = pathValue.split('/')
-				const collection = pathParts.slice(0, -1).join('/')
-				const docId = pathParts[pathParts.length - 1]
-				data[baseKey] = { __path: [collection, docId] }
-			}
-		}
-	}
-
-	if (!data.id)   data["id"] = crypto.randomUUID();
-	data["ts"] = Math.floor(Date.now() / 1000)
-}
-
 
 
 
